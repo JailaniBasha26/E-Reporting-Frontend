ef8117922f46aa1429f96e1d1fa0f4dddb625e3a
zeenath <zeenattaj@gmail.com>
2023-02-17 09:53:00 +0530

    Modifications by Zeenath

--- Frontend/src/Pages/00-Corporate Page/NavbarCorporate.js
+++ Frontend/src/Pages/00-Corporate Page/NavbarCorporate.js
@@ -1,49 +1,90 @@
-import React, {useState} from 'react'
-import { Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Link } from "react-router-dom";
 import { Image } from "primereact/image";
 import logo from "./RebelSkool_Logo.jpg";
+import { CgClose } from "react-icons/cg";
+import { GoThreeBars } from "react-icons/go";
+import ButtonCorporate from "./ButtonCorporate";
+import "./NavbarStyle.css";
 
 function NavbarCorporate() {
-  const [click,setClick] = useState(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <div>
-        <nav className='00-navbar'> 
-            <div className='00-navbar-container'>
-            <Route
-                    render={({ history }) => (
-                <button
-                type="button"
-                onClick={() => {
-                 history.push("/");
-                }}
-                class="headerButton">
+      <nav className="co-navbar">
+        <div className="co-navbar-container">
+          <Link to="/" className="co-navbar-logo" onClick={closeMobileMenu}>
             <Image
-                  src={logo}
-                  alt="Image"
-                  width="200"
-                  className="rebelSkoolLogo"
-                />
-                </button>
-            )}/>
-            <div className='00-menu-icon'>
-              {/* {click ==true
-              ?
-              <FontAwesomeIcon icon="fa-solid fa-bars" />
-              :
+              src={logo}
+              alt="Image"
+              width="200"
+              className="co-navbar-logo"
+            />
+          </Link>
 
-            } */}
-              <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-            </div>
-            <ul className={click ? '00-nav-menu-active' : '00-nav-menu'}>
+          <div className="co-menu-icon" onClick={handleClick}>
+            {click == true ? <CgClose className="co-close" /> : <GoThreeBars className="co-bars" />}
+          </div>
 
-            </ul>
-            </div>
-        </nav>
+          {/* <ul className={click ? "co-nav-menu-active" : "co-nav-menu"}>
+            <li className="co-nav-item">
+              <Link to="/" className="co-nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="co-nav-item">
+              <Link
+                to="/fileSIE"
+                className="co-nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="co-nav-item">
+              <Link
+                to="/info"
+                className="co-nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
 
-
+            <li className="co-nav-item">
+              <Link
+                to="/info"
+                className="co-nav-links"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul> */}
+          {/* {button && <ButtonCorporate buttonStyle='btn--outline'>SIGN UP</ButtonCorporate>} */}
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default NavbarCorporate
+export default NavbarCorporate;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -7,7 +7,7 @@ import Year from "./Pages/04-Year/Year";
 import home from "./Pages/01-Home/home";
 import Info from "./Pages/03-Information/Info";
 import Steps from "../src/Pages/Steps/steps";
-import Navbar from "./Pages/Navbar/Navbar";
+// import Navbar from "./Pages/Navbar/Navbar";
 
 import NavbarCorporate from "./Pages/00-Corporate Page/NavbarCorporate";
 
@@ -32,6 +32,14 @@ import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./Pages/Sidebar/Sidebar";
 import steps from "../src/Pages/Steps/steps";
+// import Navbar from "../src/Pages/Demo/Navbar";
+// import HeroSection from "../src/Pages/Demo/HeroSection";
+// import Button from "../src/Pages/Demo/Button";
+// import CardItem from "../src/Pages/Demo/CardItem";
+// import Cards from "./Pages/Demo/Cards";
+// import Subscribe from "./Pages/Demo/Subscribe";
+// import Testimonial from "./Pages/Demo/Testimonial";
+import Theme from "./Pages/Theme/Theme";
 
 // import { ThemeToggler } from "./ThemeToggler";
 // import { useDarkMode } from "./useDarkMode";
